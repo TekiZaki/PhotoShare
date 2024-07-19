0d85dd09d0a48ec545d507fa12b392a9c13af393
TekiZaki <128047436+TekiZaki@users.noreply.github.com>
2024-07-19 20:31:55 +0700

    Update upload_page.php

--- upload_page.php
+++ upload_page.php
@@ -33,9 +33,9 @@ if (!isset($_SESSION['username'])) {
     <div class="header">
         <h1>Upload Images</h1>
         <nav>
-            <a href="dashboard">Account</a>
-            <a href="display">Display</a>
-            <a href="upload_page">Upload</a>
+            <a href="dashboard.php">Account</a>
+            <a href="display.php">Display</a>
+            <a href="upload_page.php">Upload</a>
         </nav>
     </div>
     <div class="container">
